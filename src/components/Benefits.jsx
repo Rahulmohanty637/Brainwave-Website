7771fefe0502fba15116bf28858649c4e5e00bd0
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-25 22:37:42 +0530

    Benefits

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "../constants";
+import Arrow from "../assets/svg/Arrow";
 
 const Benefits = () => {
   return (
@@ -12,10 +13,34 @@ const Benefits = () => {
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
-        <div>
-            {benefits.map((item)=>(
-                <div className=""></div>
-            ))}
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="flex flex-col z-2 relative min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                    className=""
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore More
+                  </p>
+                  <Arrow />
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </Section>
