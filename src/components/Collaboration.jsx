79d09ccc23d1b5f43246113179ab8717ed557870
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-27 09:28:44 +0530

    Update Collaboration.jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import Section from "./Section";
-import { collabContent } from "../constants";
+import { collabContent, collabText } from "../constants";
 import { check } from "../assets";
 import Button from "./Button";
 
 const Collaboration = () => {
   return (
     <Section crosses>
-      <div className="max-w-[25rem]">
-        <h2 className="h2 mb-4 md:mb-8">
-          AI Chat App for Seamless Collaboration
-        </h2>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for Seamless Collaboration
+          </h2>
 
-        <ul>
-          {collabContent.map((item) => (
-            <li key={item.id} className="mb-3 py-3">
-              <div className="flex items-center">
-                <img src={check} width={24} height={24} alt="check" />
-                <h6 className="body-2 ml-5">{item.title}</h6>
-              </div>
-              {item.text && <p className="body-2 mt-3 text-n-4">{item.text}</p>}
-            </li>
-          ))}
-        </ul>
+          <ul>
+            {collabContent.map((item) => (
+              <li key={item.id} className="mb-3 py-3">
+                <div className="flex items-center">
+                  <img src={check} width={24} height={24} alt="check" />
+                  <h6 className="body-2 ml-5">{item.title}</h6>
+                </div>
+                {item.text && (
+                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                )}
+              </li>
+            ))}
+          </ul>
+          <Button>Try it Now</Button>
+        </div>
 
-        <Button>Try it Now</Button>
+        <div className="lg:ml-auto xl:max-w-[38rem] mt-4">
+          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+            {collabText}
+          </p>
+        </div>
       </div>
     </Section>
   );
