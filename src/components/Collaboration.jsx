e29cc611614d68c3871ec1bfc41d21033613be99
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-28 22:58:20 +0530

    Update Collaboration.jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Section from "./Section";
-import { collabContent, collabText } from "../constants";
-import { check } from "../assets";
+import { collabApps, collabContent, collabText } from "../constants";
+import { brainwaveSymbol, check } from "../assets";
 import Button from "./Button";
 
 const Collaboration = () => {
@@ -33,6 +33,30 @@ const Collaboration = () => {
           <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
             {collabText}
           </p>
+
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  <img
+                    src={brainwaveSymbol}
+                    alt="brainwave"
+                    width={48}
+                    height={48}
+                  />
+                </div>
+              </div>
+            </div>
+            <ul>
+              {collabApps.map((app, index) => (
+                <li key={app.id}>
+                  <div>
+                    <img src={app.icon} alt="" />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </Section>
