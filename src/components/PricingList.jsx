4a9ed904dbdd7abbf44527925f3bffbfdddbe391
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-12-01 08:23:15 +0530

    Pricing component

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { check } from "../assets";
 import { pricing } from "../constants";
 import Button from "./Button";
@@ -6,7 +6,7 @@ import Button from "./Button";
 const PricingList = () => {
   return (
     <div className="flex gap-[1rem] max-lg:flex-wrap">
-       {pricing.map((item) => (
+      {pricing.map((item) => (
         <div
           key={item.id}
           className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
@@ -26,9 +26,30 @@ const PricingList = () => {
                 </div>
               </>
             )}
-            </div>
-    </div>
-  )
-}
+          </div>
+          <Button
+            className="w-full mb-6"
+            href={item.price ? "/pricing" : "mailto:contact@jsmastery.pro"}
+            white={!!item.price}
+          >
+            {item.price ? "Get started" : "Contact us"}
+          </Button>
 
-export default PricingList
+          <ul>
+            {item.features.map((feature, index) => (
+              <li
+                key={index}
+                className="flex items-start py-5 border-t border-n-6"
+              >
+                <img src={check} width={24} height={24} alt="Check" />
+                <p className="body-2 ml-4">{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PricingList;
